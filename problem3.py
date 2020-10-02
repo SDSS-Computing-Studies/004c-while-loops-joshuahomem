5437d9012b741b6b18cfd28c083c9fecae491d2a
joshuahomem <71406869+joshuahomem@users.noreply.github.com>
2020-10-02 11:55:15 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -13,3 +13,14 @@ after the number in the sequence is greater than 100:
 Example:
 1 1 2 3 5 ...
 """
+a=1
+b=1
+print(1)
+print(1)
+while True:
+    c=a+b
+    a=b
+    b=c
+    print(c)
+    if c>55:
+        break
