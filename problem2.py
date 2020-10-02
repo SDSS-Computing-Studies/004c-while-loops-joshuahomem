65909f2f74e98f269c44148550c0dfed50644f87
joshuahomem <71406869+joshuahomem@users.noreply.github.com>
2020-10-02 11:36:57 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -15,3 +15,12 @@ example:
 Enter a number: 4
 4 8 12 16 20 24 28 32 36 40 44 48
 """
+a=input("input")
+a=int(a)
+b=1
+c=""
+while b<12:
+    b=b+1
+    c=b*a
+    
+    print(str(c) + " " , end='')
